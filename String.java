37d19e5330485157e355b0e9e41672be69e2c49e
Shubham Giri <99181962+ShubhamGiri26@users.noreply.github.com>
2023-10-18 23:45:25 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class String{
+  public static void main(String[] args){
+    String name1=Amit;
+    String name2=Ajay;
+    String name3= name1 + "and" + name2;
+    System.out.println("name3");
+  }
+}
